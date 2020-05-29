0aa85638d86a2c976a87dfbfe1a0d6e666d25ef5
@itsAj <ajaymati1000@gmail.com>
2020-05-29 22:33:53 +0530

    template ready

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 const nav_btn = document.querySelector(".nav-btn");
 const menu = document.querySelector(".menu");
 const menu_brand = document.querySelector(".menu-brand");
-const menu_items = document.querySelectorAll(".menu-item");
+const menu_items = document.querySelector(".menu-items");
 
 nav_btn.addEventListener("click", show);
 
@@ -12,17 +12,13 @@ function show() {
     nav_btn.classList.add("close");
     menu_brand.classList.add("show");
     menu.classList.add("show");
-    menu_items.forEach((menu_item) => {
-      menu_item.classList.add("show");
-    });
+    menu_items.classList.add("show");
     showed = true;
   } else {
     nav_btn.classList.remove("close");
     menu_brand.classList.remove("show");
     menu.classList.remove("show");
-    menu_items.forEach((menu_item) => {
-      menu_item.classList.remove("show");
-    });
+    menu_items.classList.remove("show");
     showed = false;
   }
 }
